f9289788c185985937e146a6dc837bdab4886814
artur <artur24814@gmail.com>
2022-12-29 23:56:18 +0100

    add functionality to playelr▶️

---
+++ utils.py
@@ -0,0 +1,31 @@
+'''
+Helper functions
+'''
+
+
+def convert_duration_to_show(duration):
+    duration_to_show = (duration / 1000) / 60
+    duration_list = str(format(duration_to_show, '.2f')).split('.')
+    return duration_list
+
+
+def get_html(time):
+    return '''
+    <!DOCTYPE HTML>
+    <html>
+    <body>
+    <table border="0" style="width: 100vw;">
+    <thead>
+    <tr>
+    <td align="left">
+    <span style="font-family:'Monaco'; font-weight:600;">▶</span></td>
+    <td align="right">
+    <span style=" font-family:'Fantasy'; font-size:28px; font-weight:600;">&nbsp;&nbsp;&nbsp;&nbsp;0%s</span>
+    </td>
+    </tr>
+    </thead>
+    </table>
+    </body>
+    </html>
+    ''' % time
+
--- winamp.py
+++ winamp.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QMediaPlaylist
 import sys
+from utils import get_html, convert_duration_to_show
 
 class UI(QMainWindow):
     def __init__(self):
@@ -21,6 +22,8 @@ class UI(QMainWindow):
         self.stop_btn = self.findChild(QPushButton, "stop_btn")
         self.next_btn = self.findChild(QPushButton, "next_btn")
         self.download_btn = self.findChild(QPushButton, "download_btn")
+        self.shuffle_btn = self.findChild(QPushButton, 'shuffle_btn')
+        self.loop_btn = self.findChild(QPushButton, 'loop_btn')
         #set icons
         self.back_btn.setIcon(self.style().standardIcon(QStyle.SP_ArrowBack))
         self.play_btn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
@@ -30,9 +33,14 @@ class UI(QMainWindow):
         self.download_btn.setIcon(self.style().standardIcon(QStyle.SP_ArrowUp))
 
         #click Buttons
+        self.back_btn.clicked.connect(self.back)
         self.play_btn.clicked.connect(self.play)
         self.pause_btn.clicked.connect(self.pause)
+        self.stop_btn.clicked.connect(self.stop)
+        self.next_btn.clicked.connect(self.next)
         self.download_btn.clicked.connect(self.download)
+        self.shuffle_btn.clicked.connect(self.shuffle)
+        self.loop_btn.clicked.connect(self.loop)
 
         #sliders
         self.time_slider = self.findChild(QSlider, 'time_slider')
@@ -43,6 +51,7 @@ class UI(QMainWindow):
         self.volume_slider.valueChanged.connect(self.set_volume)
 
         #LCD
+        self.time_lcd =self.findChild(QTextEdit, 'time_lcd')
         self.title_lcd = self.findChild(QTextEdit, 'title_lcd')
 
         #create media Player
@@ -56,34 +65,74 @@ class UI(QMainWindow):
         self.Player.positionChanged.connect(self.position_changed)
         self.Player.durationChanged.connect(self.duration_changed)
 
+        #current song
+        self.playlist.currentMediaChanged.connect(self.songChanged)
+
+        #set value for loop plaing
+        self.loop_plaing = False
+
         self.set_Enabled_button()
         #show The App
         self.show()
 
-
     def set_Enabled_button(self):
-        self.back_btn.setEnabled(False)
-        # self.play_btn.setEnabled(False)
-        self.pause_btn.setEnabled(False)
+        #when init player
+        if self.playlist.isEmpty():
+            self.back_btn.setEnabled(False)
+            self.play_btn.setEnabled(False)
+            self.pause_btn.setEnabled(False)
+            self.stop_btn.setEnabled(False)
+            self.next_btn.setEnabled(False)
+        #when we have some music in playlist
+        else:
+            self.back_btn.setEnabled(True)
+            self.play_btn.setEnabled(True)
+            self.pause_btn.setEnabled(True)
+            self.stop_btn.setEnabled(True)
+            self.next_btn.setEnabled(True)
 
+    #to previous song
+    def back(self):
+        self.playlist.previous()
+
+    #play music
     def play(self):
-        self.Player.setPlaylist(self.playlist)
-        print(self.playlist)
-        self.Player.play()
-        self.Player.setVolume(self.volume_slider.value())
-        # self.title_lcd.setPlainText(filename.split('/')[-1])
+        #if already play
+        if self.Player.state() == QMediaPlayer.PlayingState:
+            self.Player.pause()
+        else:
+            self.Player.play()
+            self.Player.setVolume(self.volume_slider.value())
 
+    #pause music
     def pause(self):
         if self.Player.state() == QMediaPlayer.PlayingState:
             self.Player.pause()
+        else:
+            self.play()
 
+    #stop music
+    def stop(self):
+        if self.Player.state() == QMediaPlayer.PlayingState:
+            self.Player.stop()
+
+    #next song
+    def next(self):
+        self.playlist.next()
+
+    #download list of music
     def download(self):
+        #open dialog window
         filenames, _ = QFileDialog.getOpenFileNames(self, 'Play song')
         if len(filenames) != 0:
             for filename in filenames:
                 url = QUrl.fromLocalFile(filename)
+                #add song to playlist
                 self.playlist.addMedia(QMediaContent(url))
-            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+            #add playlist to player
+            self.Player.setPlaylist(self.playlist)
+            #set button to enabled
+            self.set_Enabled_button()
 
     def set_volume(self, value):
         self.Player.setVolume(value)
@@ -92,18 +141,63 @@ class UI(QMainWindow):
         if self.Player.state() == QMediaPlayer.PlayingState:
             pass
 
+    #update slider position
     def position_changed(self, position):
         self.time_slider.setValue(position)
+        duration_list = convert_duration_to_show(position)
+        time = duration_list[0] + ':' + duration_list[1]
+        self.time_lcd.setHtml(get_html(time))
 
+    #set slider range
     def duration_changed(self, duration):
         self.time_slider.setRange(0, duration)
+        #add duration to song title
+        duration_list = convert_duration_to_show(duration)
+        text = self.title_lcd.toPlainText() + ' (' + duration_list[0] + ':' + duration_list[1] + ')'
+        self.title_lcd.setPlainText(text)
 
+    #set position played song
     def set_position(self, position):
         self.Player.setPosition(position)
 
+    #show title played of song in text input
+    def songChanged(self, media):
+        if not media.isNull():
+            url = media.canonicalUrl()
+            text = str(self.playlist.currentIndex() + 1 ) + '.' + url.fileName().split('/')[-1]
+            self.title_lcd.setPlainText(text)
+
+    def shuffle(self):
+        self.playlist.shuffle()
+
+    def loop(self):
+        #if mode plyilist = sequential
+        if self.loop_plaing is False:
+            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+            self.loop_btn.setStyleSheet(""" background-color : rgb(53, 159, 159);
+                                            border-top: 4px double rgb(253, 253, 253);
+                                            border-right: 4px double #DFDBDD;
+                                            border-bottom: 4px double #BCB8BA;
+                                            border-left: 4px double #EFEAEC;""")
+            self.loop_plaing = True
+        #if already play in loop
+        else:
+            self.playlist.setPlaybackMode(QMediaPlaylist.Sequential)
+            self.loop_btn.setStyleSheet("""background-color: rgb(221, 221, 221);
+                                            border-top: 4px double rgb(253, 253, 253);
+                                            border-right: 4px double #DFDBDD;
+                                            border-bottom: 4px double #BCB8BA;
+                                            border-left: 4px double #EFEAEC;""")
+            self.loop_plaing = False
+
+    #show error in TextInput
     def handle_errors(self):
         self.play_btn.setEnabled(False)
         self.title_lcd.setPlainText('Error' + str(self.Player.errorString()))
+
+    def __del__(self):
+        if hasattr(self, "playlist"):
+            del self.playlist
 
 
 
